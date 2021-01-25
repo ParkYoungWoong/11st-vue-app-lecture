c6e90ee1f5e4fa5a95055a5663b5744774589598
ParkYoungWoong <thesecon@gmail.com>
2021-01-25 16:25:23 +0900

    Working...

--- src/store/navigation.js
+++ src/store/navigation.js
@@ -1,4 +1,29 @@
 // src/store/navigation.js
 export default {
-  namespaced: true
+  namespaced: true,
+  state: () => ({
+    isShowLNB: false
+  }),
+  mutations: {
+    updateState (state, payload) {
+      // ['isShowLNB']
+      Object.keys(payload).forEach(key => {
+        // state.isShowLNB = payload.isSHowLNB
+        state[key] = payload[key]
+      })
+    }
+  },
+  actions: {
+    onNav ({ commit }) {
+      commit('updateState', {
+        isShowLNB: true
+      })
+      // isShowLNB => true
+    },
+    offNav ({ commit }) {
+      commit('updateState', {
+        isShowLNB: false
+      })
+    }
+  }
 }
