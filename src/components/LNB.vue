a88f30dce3f0b32bdce6b1621c4cf4819dad0725
ParkYoungWoong <thesecon@gmail.com>
2021-01-25 17:20:33 +0900

    Working...

--- src/components/LNB.vue
+++ src/components/LNB.vue
@@ -1,11 +1,39 @@
 <template>
   <div>
-    <nav :class="{ show: isShowLNB }">
+    <nav
+      v-if="done"
+      :class="{ show: isShowLNB }">
       <div class="user">
         <a href="javascript:void(0)">로그인</a>
+        <div class="flex-space"></div>
         <div
           class="close-nav"
           @click="offNav"></div>
+      </div>
+      <div class="container">
+        <!--GROUP-->
+        <div class="group categories">
+          <h3 class="group__title">
+            {{ navigations.categories.title }}
+          </h3>
+          <ul class="group__list">
+            <li
+              v-for="(item1, index) in navigations.categories.list"
+              :key="item1.name">
+              <div class="category-icon"></div>
+              {{ item1.name }}
+              <ul class="depth">
+                <li
+                  v-for="item2 in item1.list"
+                  :key="item2.name">
+                  <a :href="item2.href">
+                    {{ item2.name }}
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
 
@@ -18,14 +46,31 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data () {
+    return {
+      navigations: {},
+      done: false
+    }
+  },
   computed: {
     isShowLNB () {
       // this.$store.state.네임스페이스.상태
       return this.$store.state.navigation.isShowLNB
     }
   },
+  created () {
+    this.init()
+  },
   methods: {
+    async init () {
+      this.navigations = await this.$fetch({
+        requestName: 'navigations'
+      })
+      this.done = true
+    },
     offNav () {
       this.$store.dispatch('navigation/offNav')
     }
@@ -49,6 +94,22 @@ export default {
       transform: translateX(0);
     }
     .user {
+      height: 70px;
+      padding: 0 25px;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      a {
+        font-size: 20px;
+        font-weight: 700;
+        text-decoration: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+      .flex-space {
+        flex-grow: 1;
+      }
       .close-nav {
         width: 36px;
         height: 36px;
@@ -56,6 +117,16 @@ export default {
         background-position: -261px -203px;
         background-size: 363px;
         cursor: pointer;
+      }
+    }
+    .container {
+      .group {
+        &__title {
+
+        }
+        &__list {
+
+        }
       }
     }
   }
